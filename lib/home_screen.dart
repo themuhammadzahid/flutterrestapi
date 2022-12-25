d4bd2bfd4098dbb74712a1313439c7a24fdc7441
abzahid <abdul.zahid@capgemini.com>
2022-12-25 15:17:35 +0530

    added function for calling api

---
+++ lib/home_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'models/PostModel.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postModel = [];
+  Future<List<PostModel>> getUserList() async {
+    var response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postModel.add(PostModel.fromJson(i));
+      }
+      return postModel;
+    } else {
+      return postModel;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("REST API"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getUserList(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text("loading...");
+                  } else {
+                    return ListView.builder(
+                      itemCount: postModel.length,
+                        itemBuilder: (context, index) {
+                      return Text(postModel[index].body.toString());
+                    });
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
